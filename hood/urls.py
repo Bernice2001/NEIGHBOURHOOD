ed4500b16fbb9008d40519d4a143e2de8a69fde9
Bernice2001 <wangechibanim@gmail.com>
2020-11-05 08:52:02 +0300

    add the bg image

---
+++ hood/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    path('', views.index, name='index'),
+    # path('neighbourhood/<int:id>/', views.index, name='index'),
+    # path('signup/', views.signup, name='signup'),
+    # path('logout/', views.logout, name='logout'),
+    # path('accounts/login', views.signin, name='login'),
+    # path('profile/<username>/', views.profile, name='profile'),
+    # path('business_list/', views.business, name='business'),
+    # path('post/', views.post_news, name='post'),
+    # path('new_hood/<username>/', views.neighbourhood, name='hood'),
+    # path('welcome', views.welcome, name='welcome'),
+] 
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
